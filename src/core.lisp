478051530b20efa8bd4afc820e81f5c80597015a
Dimercel <xolcman@gmail.com>
2019-04-29 16:20:04 +0300

    Basic operations with fractions

--- src/core.lisp
+++ src/core.lisp
@@ -9,8 +9,27 @@
   (num 1)
   (denom 1))
 
-(defun reduce-fraction (fraction)
-  (let ((gcd-val (gcd (fraction-num fraction)
-                      (fraction-denom fraction))))
-    (make-fraction :num (/ (fraction-num fraction) gcd-val)
-                   :denom (/ (fraction-denom fraction) gcd-val))))
+(defun reduce-fraction (frac)
+  (let ((gcd-val (gcd (fraction-num frac)
+                      (fraction-denom frac))))
+    (make-fraction :num (/ (fraction-num frac) gcd-val)
+                   :denom (/ (fraction-denom frac) gcd-val))))
+
+(defun add (x y)
+  (reduce-fraction
+   (make-fraction :num (+ (* (fraction-num x) (fraction-denom y))
+                          (* (fraction-num y) (fraction-denom x)))
+                  :denom (* (fraction-denom x) (fraction-denom y)))))
+
+(defun sub (x y)
+  (add x (make-fraction :num (- (fraction-num y))
+                        :denom (fraction-denom y))))
+
+(defun mul (x y)
+  (reduce-fraction
+   (make-fraction :num (* (fraction-num x) (fraction-num y))
+                  :denom (* (fraction-denom x) (fraction-denom y)))))
+
+(defun div (x y)
+  (mul x (make-fraction :num (fraction-denom y)
+                        :denom (fraction-num y))))
